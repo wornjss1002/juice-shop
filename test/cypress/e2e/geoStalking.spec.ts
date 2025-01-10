f9e77233b0c7f810293e967feddaf18a7ad723b6
DevOpsSCP <wornjss102@gmail.com>
2025-01-10 10:23:02 +0900

    Update geoStalking.spec.ts

--- test/cypress/e2e/geoStalking.spec.ts
+++ test/cypress/e2e/geoStalking.spec.ts
@@ -5,11 +5,13 @@ describe('/#/photo-wall', () => {
 
   describe('challenge "geoStalkingMeta"', () => {
     it('Should be possible to find the answer to a security question in the meta-data of a photo on the photo wall', () => {
-      cy.task<string>('GetFromMemories', 'geoStalkingMetaSecurityAnswer').then(
-        (answer: string) => {
-          cy.task('GetFromConfig', 'application.domain').then((appDomain: string) => {
-            cy.get('#email').type(`john@${appDomain}`)
-            cy.get('#securityAnswer').type(answer)
+      cy.task('GetFromMemories', 'geoStalkingMetaSecurityAnswer').then(
+        (answer: unknown) => {
+          const answerString = answer as string;
+          cy.task('GetFromConfig', 'application.domain').then((appDomain: unknown) => {
+            const appDomainString = appDomain as string;
+            cy.get('#email').type(`john@${appDomainString}`)
+            cy.get('#securityAnswer').type(answerString)
             cy.get('#newPassword').type('123456')
             cy.get('#newPasswordRepeat').type('123456')
             cy.get('#resetButton').click()
@@ -23,11 +25,13 @@ describe('/#/photo-wall', () => {
 
   describe('challenge "geoStalkingVisual"', () => {
     it('Should be possible to determine the answer to a security question by looking closely at an image on the photo wall', () => {
-      cy.task<string>('GetFromMemories', 'geoStalkingVisualSecurityAnswer').then(
-        (answer: string) => {
-          cy.task('GetFromConfig', 'application.domain').then((appDomain: string) => {
-            cy.get('#email').type(`emma@${appDomain}`)
-            cy.get('#securityAnswer').type(answer)
+      cy.task('GetFromMemories', 'geoStalkingVisualSecurityAnswer').then(
+        (answer: unknown) => {
+          const answerString = answer as string;
+          cy.task('GetFromConfig', 'application.domain').then((appDomain: unknown) => {
+            const appDomainString = appDomain as string;
+            cy.get('#email').type(`emma@${appDomainString}`)
+            cy.get('#securityAnswer').type(answerString)
             cy.get('#newPassword').type('123456')
             cy.get('#newPasswordRepeat').type('123456')
             cy.get('#resetButton').click()
